3015c59dff5a8f018ba081d7953031470b17ebaa
Shengteng Ouyang <shengtengouyang@gmail.com>
2020-08-08 19:11:02 -0400

    fix vote, update properties with h2&datasource setup

--- src/main/java/com/shen/redditclone/domain/Vote.java
+++ src/main/java/com/shen/redditclone/domain/Vote.java
@@ -4,12 +4,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Vote {
-    
+    @Id
+    @GeneratedValue
     private long id;
     private int vote;
 
