fdc4f05d7f61e6d6d2a7889ff67f81047cd4bb60
Shengteng Ouyang <shengtengouyang@gmail.com>
2020-08-11 20:21:32 -0400

    setup initial controller

---
+++ src/main/java/com/shen/redditclone/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.shen.redditclone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello, Spring Boot 2!";
+    }
+}
