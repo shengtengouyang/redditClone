b30edaf47877fde9c6161a94f5b52c9f82d678a1
Shengteng Ouyang <shengtengouyang@gmail.com>
2020-09-13 20:15:25 -0400

    add jpaConfig, allow h2 access when development

--- src/main/java/com/shen/redditclone/RedditcloneApplication.java
+++ src/main/java/com/shen/redditclone/RedditcloneApplication.java
@@ -7,13 +7,10 @@ import com.shen.redditclone.repositery.LinkRepository;
 import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class RedditcloneApplication {
     private static final Logger log = LoggerFactory.getLogger(RedditcloneApplication.class);
--- src/main/java/com/shen/redditclone/security/SecurityConfiguration.java
+++ src/main/java/com/shen/redditclone/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.shen.redditclone.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,10 +20,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
                 .antMatchers("/").permitAll()
                 .antMatchers("/link/submit").hasRole("USER")
+                .antMatchers("/h2-console/**").permitAll()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
     }
 
     @Override
